09bffd70059315a547a0536645c638c40f321f7c
kanison konkla <53283017+pooooooon@users.noreply.github.com>
2021-10-24 14:52:32 +0700

    Update StartGamePacket.java

--- src/main/java/org/barrelmc/barrel/network/translator/bedrock/StartGamePacket.java
+++ src/main/java/org/barrelmc/barrel/network/translator/bedrock/StartGamePacket.java
@@ -1,7 +1,12 @@
 package org.barrelmc.barrel.network.translator.bedrock;
 
+import com.github.steveice10.mc.protocol.packet.ingame.server.ServerDifficultyPacket;
+import com.github.steveice10.mc.protocol.data.game.entity.EntityStatus;
 import com.github.steveice10.mc.protocol.packet.ingame.server.ServerJoinGamePacket;
 import com.github.steveice10.mc.protocol.packet.ingame.server.entity.player.ServerPlayerPositionRotationPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.server.entity.ServerEntityStatusPacket;
+import com.github.steveice10.mc.protocol.packet.ingame.server.ServerPluginMessagePacket;
+import com.github.steveice10.mc.protocol.data.game.setting.Difficulty;
 import com.nukkitx.math.vector.Vector2f;
 import com.nukkitx.math.vector.Vector3f;
 import com.nukkitx.protocol.bedrock.BedrockPacket;
@@ -10,7 +15,7 @@ import org.barrelmc.barrel.network.translator.TranslatorUtils;
 import org.barrelmc.barrel.network.translator.interfaces.BedrockPacketTranslator;
 import org.barrelmc.barrel.player.Player;
 import org.barrelmc.barrel.server.ProxyServer;
-
+import java.nio.charset.StandardCharsets;
 public class StartGamePacket implements BedrockPacketTranslator {
 
     @Override
@@ -22,16 +27,27 @@ public class StartGamePacket implements BedrockPacketTranslator {
                 TranslatorUtils.translateGamemodeToJE(packet.getPlayerGameType()),
                 TranslatorUtils.translateGamemodeToJE(packet.getPlayerGameType()),
                 1, new String[]{"minecraft:world"}, ProxyServer.getInstance().getDimensionTag(),
-                ProxyServer.getInstance().getOverworldTag(), "minecraft:world", 100,
-                0, 16, false, true, false, false
+                ProxyServer.getInstance().getOverworldTag(), "minecraft:world", packet.getSeed(),
+                0, 16, true, true, true, false
         );
-
+        Difficulty difficulty;
+        try {
+            difficulty = convertDifficultyToJE(packet.getDifficulty());
+        } catch (Exception e) {
+            difficulty = Difficulty.NORMAL;
+        }
+        ServerDifficultyPacket serverDifficultyPacket = new ServerDifficultyPacket(difficulty, true);
+        ServerEntityStatusPacket serverEntityStatusPacket = new ServerEntityStatusPacket((int) packet.getRuntimeEntityId(), EntityStatus.PLAYER_OP_PERMISSION_LEVEL_0);
         Vector3f position = packet.getPlayerPosition();
         Vector2f rotation = packet.getRotation();
-        ServerPlayerPositionRotationPacket serverPlayerPositionRotationPacket = new ServerPlayerPositionRotationPacket(position.getX(), position.getY(), position.getZ(), rotation.getY(), rotation.getX(), 1);
-
+        ServerPlayerPositionRotationPacket serverPlayerPositionRotationPacket = new ServerPlayerPositionRotationPacket(position.getX(), position.getY(), position.getZ(), rotation.getY(), rotation.getX(), 1, true);
+        ServerPluginMessagePacket serverPluginMessagePacket = new ServerPluginMessagePacket("minecraft:brand", packet.getServerEngine().getBytes(StandardCharsets.UTF_8));
+        
         player.getJavaSession().send(serverJoinGamePacket);
+        player.getJavaSession().send(serverPluginMessagePacket);
+        player.getJavaSession().send(serverDifficultyPacket);
         player.getJavaSession().send(serverPlayerPositionRotationPacket);
+        player.getJavaSession().send(serverEntityStatusPacket);
 
         SetLocalPlayerAsInitializedPacket setLocalPlayerAsInitializedPacket = new SetLocalPlayerAsInitializedPacket();
         setLocalPlayerAsInitializedPacket.setRuntimeEntityId(packet.getRuntimeEntityId());
@@ -39,4 +55,28 @@ public class StartGamePacket implements BedrockPacketTranslator {
 
         player.setRuntimeEntityId((int) packet.getRuntimeEntityId());
     }
+    
+    public static Difficulty convertDifficultyToJE(int diff) {
+        Difficulty difficulty;
+        switch (diff) {
+            case 0: {
+                difficulty = Difficulty.PEACEFUL;
+                break;
+            }
+            case 1: {
+                difficulty = Difficulty.EASY;
+                break;
+            }
+            case 3: {
+                difficulty = Difficulty.HARD;
+                break;
+            }
+            default: {
+                difficulty = Difficulty.NORMAL;
+                break;
+            }
+        }
+        return difficulty;
+    }
+    
 }
